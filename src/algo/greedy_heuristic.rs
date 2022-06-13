04a8bf7c57d734b7a9daa81dd605bd3934a60d7a
Boerdy <boerdy4development@outlook.de>
2022-06-13 13:04:31 +0200

    #12 More complex unit test. Need to refactor to generic test (Graph, Algo and Dot-bool as params).

--- src/algo/greedy_heuristic.rs
+++ src/algo/greedy_heuristic.rs
@@ -20,8 +20,11 @@ impl FeedbackArcSet for GreedyHeuristic {
 mod tests {
   use crate::algo::greedy_heuristic::GreedyHeuristic;
   use crate::feedback_arc_set::FeedbackArcSet;
-  use petgraph::graph::{DiGraph, EdgeIndex};
+  use petgraph::algo::is_cyclic_directed;
+  use petgraph::dot::{Config, Dot};
+  use petgraph::graph::{DiGraph, EdgeIndex, EdgeReference};
   use petgraph::visit::EdgeRef;
+  use petgraph::Graph;
 
   #[test]
   fn deterministic_on_simple_clique() {
@@ -31,5 +34,72 @@ mod tests {
 
     assert_eq!(edges.len(), 1);
     assert_eq!(edges.get(0).unwrap().id(), EdgeIndex::new(1));
+  }
+
+  #[test]
+  fn works_on_multiple_cliques() {
+    let cyclic_graph = DiGraph::<i32, ()>::from_edges(&[
+      (0, 1),
+      (0, 7),
+      (1, 2),
+      (1, 3),
+      (2, 4),
+      (2, 5),
+      (2, 6),
+      (3, 7),
+      (6, 8),
+      (6, 9),
+      (7, 9),
+      (5, 10),
+      (8, 10),
+      (9, 10),
+      (4, 11),
+      (4, 12),
+      (12, 11),
+      (10, 13),
+      (11, 13),
+      (10, 14),
+      (14, 15),
+      (14, 16),
+      (16, 15),
+      (16, 17),
+      (17, 18),
+      (12, 18),
+      // Ab hier kommen Zyklen rein
+      (13, 2),
+      (7, 1),
+      (6, 7),
+      (15, 10),
+      (15, 13),
+    ]);
+
+    print_dot(&cyclic_graph);
+
+    let removable_edges = GreedyHeuristic {}.compute(&cyclic_graph);
+    let acyclic_graph = remove_edges(&cyclic_graph, &removable_edges);
+
+    print_dot(&acyclic_graph);
+
+    assert_eq!(removable_edges.len(), 4);
+    assert!(!is_cyclic_directed(&acyclic_graph));
+  }
+
+  fn remove_edges(
+    cyclic_graph: &Graph<i32, ()>,
+    removable_edges: &Vec<EdgeReference<()>>,
+  ) -> Graph<i32, ()> {
+    let mut acyclic_graph = cyclic_graph.clone();
+    for edge in removable_edges.as_slice() {
+      acyclic_graph.remove_edge(edge.id());
+    }
+    acyclic_graph
+  }
+
+  fn print_dot(graph: &Graph<i32, ()>) {
+    println!(
+      "{:?}",
+      // zeigt in IntelliJ Fehler (Dot doesn't implement Debug) an, aber läuft.
+      Dot::with_config(graph, &[Config::EdgeNoLabel, Config::NodeIndexLabel])
+    );
   }
 }
