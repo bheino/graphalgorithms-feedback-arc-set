2b53edd27c64cc01716d5a43f0ad25425fde6b32
Boerdy <boerdy4development@outlook.de>
2022-08-15 14:32:07 +0200

    Some corrections. Move problem exists

--- src/bisection/stochastic_evolution.rs
+++ src/bisection/stochastic_evolution.rs
@@ -40,11 +40,12 @@ pub struct StochasticEvolution<'a> {
 
 impl<'a> StochasticEvolution<'a> {
   pub fn new(graph: &'a HashTable) -> Self {
+    let vertices = graph.vertices();
     Self {
       graph,
-      current_bisection: initial_bisection(graph.vertices().len()),
-      best_bisection: initial_bisection(graph.vertices().len()),
-      vertices: graph.vertices(),
+      current_bisection: initial_bisection(vertices.len()),
+      best_bisection: initial_bisection(vertices.len()),
+      vertices,
     }
   }
 
@@ -80,18 +81,12 @@ impl<'a> StochasticEvolution<'a> {
       }
     }
 
-    self.dummy_result()
-  }
-
-  fn dummy_result(&self) -> (HashSet<VertexId>, HashSet<VertexId>) {
-    let vertices = self.graph.vertices();
+    let bisection = |vec: &Vec<usize>| -> HashSet<VertexId> {
+      vec.iter().map(|idx| self.vertices[*idx]).collect()
+    };
     (
-      HashSet::from_iter(vertices[0..(vertices.len() / 2)].iter().cloned()),
-      HashSet::from_iter(
-        vertices[(vertices.len() / 2)..vertices.len()]
-          .iter()
-          .cloned(),
-      ),
+      bisection(&self.best_bisection.0),
+      bisection(&self.best_bisection.1),
     )
   }
 
@@ -113,10 +108,10 @@ impl<'a> StochasticEvolution<'a> {
     let bisection;
     let mut stack;
     if self.current_bisection.0.len() > self.current_bisection.1.len() {
-      bisection = self.current_bisection.0.clone();
+      bisection = &self.current_bisection.0;
       stack = s1;
     } else {
-      bisection = self.current_bisection.1.clone();
+      bisection = &self.current_bisection.1;
       stack = s2;
     }
 
@@ -129,11 +124,14 @@ impl<'a> StochasticEvolution<'a> {
   // Number of Edges from Partition 2 to Partition 1
   fn cost(&mut self) -> usize {
     let mut cost = 0;
-    for v_2_index in self.current_bisection.1.as_slice() {
+
+    let bisection_2 = &self.current_bisection.1;
+    for v_2_index in bisection_2 {
       let v_2 = self.vertices[*v_2_index];
       for (_, neighbour) in self.graph.edges(v_2, Direction::Outbound) {
+        let neighbor_pos = self.vertices.iter().position(|v| *v == neighbour).unwrap();
         // check if neighbor is in other partition
-        if self.vertices.contains(&neighbour) {
+        if self.current_bisection.0.contains(&neighbor_pos) {
           cost += 1;
         }
       }
@@ -142,28 +140,38 @@ impl<'a> StochasticEvolution<'a> {
     cost
   }
 
-  fn gain(&mut self, p0: usize) -> i32 {
-    // TODO
-    0
+  // Returns the reduction in cost, if move(i) would be executed.
+  fn gain(&mut self, i: usize) -> i32 {
+    let cost_current = self.cost();
+    self.move_vertex(i);
+
+    let cost_if_moved = self.cost();
+    // Reverse previous move
+    self.move_vertex(i);
+
+    // Gain can be negative. Cost not.
+    cost_current as i32 - cost_if_moved as i32
   }
 
   fn move_vertex(&mut self, i: usize) {
     let v_1 = &mut self.current_bisection.0;
     let v_2 = &mut self.current_bisection.1;
-    /* TODO debug_assert!(
+    debug_assert!(
       (v_1.contains(&i) && !v_2.contains(&i)) || (!v_1.contains(&i) && v_2.contains(&i)),
       "Vertex is on both or in none partition!"
-    );*/
+    );
 
     let is_in_v_1_position = v_1.iter().position(|v_i| *v_i == i);
     let is_in_v_2_position = v_2.iter().position(|v_i| *v_i == i);
 
     if let Some(pos) = is_in_v_1_position {
-      v_1.remove(pos);
+      let to_move = v_1.remove(pos);
+      v_2.push(to_move);
     } else if let Some(pos) = is_in_v_2_position {
-      v_2.remove(pos);
+      let to_move = v_2.remove(pos);
+      v_1.push(to_move);
     } else {
-      //TODO panic!("Moving vertex between partitions not possible, because exists in none of them");
+      panic!("Moving vertex between partitions not possible, because exists in none of them");
     }
   }
 }
@@ -191,5 +199,49 @@ mod tests {
     assert!(partition_1.is_subset(&graph_vertices));
     assert!(partition_2.is_subset(&graph_vertices));
     assert!(partition_1.is_disjoint(&partition_2));
+  }
+
+  #[test]
+  fn move_vertex_works() {
+    let graph = graph_from_wikipedia_scc();
+    let mut algorithm = StochasticEvolution::new(&graph);
+
+    let i_to_move = 3;
+
+    assert!(algorithm.current_bisection.0.contains(&i_to_move));
+    assert!(!algorithm.current_bisection.1.contains(&i_to_move));
+
+    algorithm.move_vertex(i_to_move);
+
+    assert!(!algorithm.current_bisection.0.contains(&i_to_move));
+    assert!(algorithm.current_bisection.1.contains(&i_to_move));
+  }
+
+  #[test]
+  fn gain_works() {
+    let graph = graph_from_wikipedia_scc();
+    let mut algorithm = StochasticEvolution::new(&graph);
+
+    let i_to_move = 3;
+
+    let gain = algorithm.gain(i_to_move);
+    let gain_back = algorithm.gain(i_to_move);
+
+    // TODO doing gain two times is not the same (maybe because cost behaves unstable...)
+    assert_eq!(gain + gain_back, 0);
+    assert_eq!(gain.abs(), gain_back.abs());
+  }
+
+  #[test]
+  fn cost_works() {
+    let graph = graph_from_wikipedia_scc();
+    let mut algorithm = StochasticEvolution::new(&graph);
+
+    let cost_1 = algorithm.cost();
+    let cost_2 = algorithm.cost();
+
+    assert_eq!(cost_1, cost_2);
+    // TODO cost is different on each test run. Why?
+    // assert_eq!(cost_1, 4);
   }
 }
