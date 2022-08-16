82a5b9260b7b85db788f7cf771b29aef65c04291
Jens Plüddemann <jens@plueddemann.de>
2022-08-16 13:09:11 +0200

    added more benchmark files

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -12,11 +12,14 @@ pub fn file_benchmarks(c: &mut Criterion) {
     c,
     "File Benchmarks",
     [
+      h_001, h_003, h_005, h_007, h_009, h_011, h_013, h_015, h_017, h_019, h_021, h_023, h_025,
+      h_027, h_029
+    ],
+    [
       SimpleHeuristic,
       GreedyHeuristic,
       DivideAndConquerByOrderHeuristic
-    ],
-    [h_001, h_025]
+    ]
   );
 }
 
@@ -32,22 +35,22 @@ macro_rules! generate_benchmarks {
   (
     $bencher: expr,
     $name: expr,
-    [$($algo: ident),*],
+    [$($file_name: ident),*],
     $args:tt
   ) => {
-    let mut group = $bencher.benchmark_group($name.to_string());
-    group.sampling_mode(criterion::SamplingMode::Flat);
     $(
-      generate_benchmarks!(@call group, $algo, $args);
+      let mut group = $bencher.benchmark_group(stringify!($file_name));
+      let cyclic_graph = graph_from_file(stringify!($file_name));
+      generate_benchmarks!(@call group, cyclic_graph, $file_name, $args);
+      group.finish();
     )*
-    group.finish()
+
   };
-  (@call $bencher:expr, $algo:ident, [$($file_name:ident),*]) => {
+  (@call $bencher:expr, $graph:expr, $file_name:ident, [$($algo:ident),*]) => {
     paste::paste! {
       $(
-        let cyclic_graph = graph_from_file(stringify!($file_name));
         let algo = $algo {
-          graph: &cyclic_graph,
+          graph: &$graph,
         };
         $bencher.bench_function(stringify!([<$algo _$file_name>]), |b| {
           b.iter(|| criterion::black_box(algo.feedback_arc_set()))
